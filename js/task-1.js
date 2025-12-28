f97bb91f50b17388bf45fd22944f82cab2fa36b4
Artem Strelnikov <piramida1237@gmail.com>
2025-12-28 20:51:45 +0100

    28.12 finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,5 @@
 const allList = document.querySelector("#categories");
 
-console.log(allList.children);
+console.log("Numbers of categories:", allList.children.length);
 
-console.log(`Numbers of categories: ${allList.children.length}`);
-
-console.log(`Categories: ${allList}`);
-
-const newEl = document.createElement("li");
-newEl.classList.add("item");
-
-const newTitleEl = document.createElement("h2");
-newTitleEl.textContent = "Cars";
-
-newEl.append(newTitleEl);
-allList.append(newEl);
-console.log(newEl);
+[...allList.children];
