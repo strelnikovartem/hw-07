36125cd1f4db7921f880697de4c9724311152cc0
Artem Strelnikov <piramida1237@gmail.com>
2026-01-03 20:16:27 +0100

    up

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@ const spanEl = document.querySelector("#name-output");
 inputEl.addEventListener("input", renameName);
 
 function renameName(event) {
-  // const value = event.target.value.trim();
-  // spanEl.textContent = value === "" ? "Anonymous" : value;
+  const value = event.target.value.trim();
+  spanEl.textContent = value === "" ? "Anonymous" : value;
   console.log("event");
 }
